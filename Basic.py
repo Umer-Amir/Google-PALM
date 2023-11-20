14c6505695286fc813f4ca542f698bad9f23ab1c
Umer Amir <bscs2112241@szabist.pk>
2023-11-20 16:12:05 +0500

    first prompt

---
+++ Basic.py
@@ -0,0 +1,25 @@
+import pprint
+import google.generativeai as palm
+
+palm.configure(api_key='AIzaSyDhZvuMa_Irg6kvJoaR8az1hgTijw1bodM')
+
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
+print("MODEL:" +model + "\n\n")
+
+prompt = """
+Write in great detail about the philosphy of consciousness. 
+Use multiple perspectives to describe the nature of consciousness. 
+Defend each perspective with examples and counterexamples. 
+I need atleast 500 words. 
+"""
+
+completion = palm.generate_text(
+    model=model,
+    prompt=prompt,
+    temperature=0,
+    # The maximum length of the response
+    max_output_tokens=800,
+)
+
+print(completion.result)
